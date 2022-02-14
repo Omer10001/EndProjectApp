af62201fb159362fba656a26be88427d071b1cdf
User <User@LAB2-5>
2022-02-14 14:32:16 +0200

    14.2

--- EndProjectApp/ViewModels/LoginPageVM.cs
+++ EndProjectApp/ViewModels/LoginPageVM.cs
@@ -139,7 +139,7 @@ namespace EndProjectApp.ViewModels
                         App theApp = (App)App.Current;
                         theApp.CurrentUser = user;
 
-                        Page p = new Views.MainPage();
+                        Page p = new Views.MainTabbedPage();
                         await App.Current.MainPage.Navigation.PushAsync(p);
 
 
